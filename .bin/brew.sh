f2e807782b8e170225ddbb645f2c38b266d7ab4b
William Meger <wmeger14@gmail.com>
2017-10-22 16:37:03 -0400

    added more to brew

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-# homebrew everything
+
+# Install command-line tools with homebrew
+
+# use latest
+brew update
+
+# upgrade existing
+brew upgrade
+
+# editor
+brew install vim --with-override-system-vi
+
+# other useful binaries
+brew install git
+brew install node
+brew install tree
+
+# android tools
+brew install android-platform-tools
+brew install maven
+brew install gradle
+brew install pidcat #color dat logcat
+
+# remove outdated versions from cellar
+brew cleanup
