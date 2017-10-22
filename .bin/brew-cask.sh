76c4a63ee97e59558b4e2a4398799c2907b5b27f
William Meger <wmeger14@gmail.com>
2017-10-22 16:47:22 -0400

    adding cask

---
+++ .bin/brew-cask.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install native apps
+brew install caskroom/cask/brew-cask
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install atom
+brew cask install virtualbox
+brew cask install vagrant
+
+brew install wget
+
+
+
+
