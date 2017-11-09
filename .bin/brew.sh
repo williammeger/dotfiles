d816e1303f3ac3a0b8df2292327913c91aa413f7
William Meger <wmeger14@gmail.com>
2017-11-09 15:26:58 -0500

    bash4 and cask autocomplete

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -11,11 +11,18 @@ brew upgrade
 # editor
 brew install vim --with-override-system-vi
 
+# Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+
+brew install bash-completion
+
+brew install homebrew/completions/brew-cask-completion
+
 # other useful binaries
 brew install git
 brew install node
 brew install tree
-brew install bash-completion
 
 # android tools
 brew install android-platform-tools
